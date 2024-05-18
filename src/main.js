2c51558e98ead4bac4f9aa3fe645b44b690c3193
Cecilia Kembring <ceke2200@student.miun.se>
2024-05-18 10:32:50 +0200

    matomo tracking

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueMatomo from 'vue-matomo'
 
 const app = createApp(App)
 
 app.use(router)
 
+app.use(VueMatomo, {
+    host: '{YOUR_MATOMO_INSTANCE_URL}',
+    siteId: {YOUR_SITE_ID},
+})
+
 app.mount('#app')
-
-
