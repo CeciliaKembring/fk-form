3dfaf0ccfa31b6df45fe26bf4a7da41a16dbfd8e
Cecilia Kembring <ceke2200@student.miun.se>
2024-05-18 10:26:54 +0200

    Matomo trackinig

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import VueMatomo from 'vue-matomo'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+createApp(App)
+.use(VueMatomo, {
+    // Configure your matomo server and site by providing
+    host: '{YOUR_MATOMO_INSTANCE_URL}',
+    siteId: {YOUR_SITE_ID},
+})
+.mount('#app')
 
-app.use(router)
 
-app.mount('#app')
+window._paq.push(['trackPageView']); //To track pageview
 
 
